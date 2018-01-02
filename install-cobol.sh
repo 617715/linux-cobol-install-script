4e7095a187e1fa9c8d6f10bdd64123ab1e004f8f
Gillisdc <gillisdeclercq@gmail.com>
2018-01-02 15:23:51 +0100

    Create install-cobol.sh
    initial commit

---
+++ install-cobol.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# A Bash script to install COBOL on linux
+#
+sudo apt-get -y update
+sudo apt-get -y -f dist-upgrade 
+#install GIT
+sudo apt-get -y -f install git
+#Clone the repo
+cd
+git clone https://github.com/neopragma/provision-cobol-dev-ubuntu
+ #install aptitude
+sudo apt-get install aptitude
+#install open-cobol
+sudo apt-get install open-cobol
+#copy cobol compiler to local bin for setup script verification sudo cp /usr/bin/cobc /usr/local/bin/
+
+#Run the setup script 
+cd ~/provision-cobol-dev-ubuntu ./setup
+#copy cobc to the gnu-cobol map
+sudo mkdir -p ~/.cobol/gnu-cobol-1.1/cobc
+sudo cp /usr/local/bin/cobc ~/.cobol/gnu-cobol-1.1/cobc
+
+#example compilation COBOL files
+#cobc hello_world.cbl
