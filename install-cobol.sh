624726e764c3e580e93293011a11628417119e92
Gillisdc <gillisdeclercq@gmail.com>
2018-01-02 15:36:10 +0100

    Update install-cobol.sh
    removed sudo from script

--- install-cobol.sh
+++ install-cobol.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 # A Bash script to install COBOL on linux
 #
-sudo apt-get -y update
-sudo apt-get -y -f dist-upgrade 
+apt-get -y update
+apt-get -y -f dist-upgrade 
 #install GIT
-sudo apt-get -y -f install git
+apt-get -y -f install git
 #Clone the repo
 cd
 git clone https://github.com/neopragma/provision-cobol-dev-ubuntu
- #install aptitude
-sudo apt-get install aptitude
+#install aptitude
+apt-get install aptitude
 #install open-cobol
-sudo apt-get install open-cobol
+apt-get install open-cobol
 #copy cobol compiler to local bin for setup script verification sudo cp /usr/bin/cobc /usr/local/bin/
 
 #Run the setup script 
 cd ~/provision-cobol-dev-ubuntu
-sudo ./setup
+./setup
 #copy cobc to the gnu-cobol map
-sudo mkdir -p ~/.cobol/gnu-cobol-1.1/cobc
-sudo cp /usr/local/bin/cobc ~/.cobol/gnu-cobol-1.1/cobc
+mkdir -p ~/.cobol/gnu-cobol-1.1/cobc
+cp /usr/local/bin/cobc ~/.cobol/gnu-cobol-1.1/cobc
 
 #example compilation COBOL files
 #cobc hello_world.cbl
